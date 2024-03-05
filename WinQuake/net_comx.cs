a629b4b2f794c8908e181fc8cdd70a0e5ee0e592
Är detta mitt namn? <87585550+OMGOMGplays@users.noreply.github.com>
2024-03-05 15:21:35 +0100

    net_comx

---
+++ WinQuake/net_comx.cs
@@ -0,0 +1,126 @@
+﻿namespace Quake;
+
+public unsafe class net_comx_c
+{
+    public const int NUM_COM_PORTS = 2;
+
+    public const int ERR_TTY_LINE_STATUS = -1;
+    public const int ERR_TTY_MODEM_STATUS = -2;
+    public const int ERR_TTY_NODATA = -3;
+
+    public const int QUEUESIZE = 8192;
+    public const int QUEUEMASK = QUEUESIZE - 1;
+
+    public struct queue
+    {
+        public volatile int head;
+        public volatile int tail;
+        public volatile byte* data;
+    }
+
+    public static void FULL(queue q)
+    {
+        q.head = ((q.tail - 1) & QUEUEMASK);
+    }
+
+    public static void EMPTY(queue q)
+    {
+        q.tail = q.head;
+    }
+
+    public static void ENQUEUE(queue q, byte b)
+    {
+        q.data[q.head] = b;
+        q.head = (q.head + 1) & QUEUEMASK;
+    }
+
+    public static void DEQUEUE(queue q, byte b)
+    {
+        b = q.data[q.tail];
+        q.tail = (q.tail + 1) & QUEUEMASK;
+    }
+
+    public const int TRANSMIT_HOLDING_REGISTER = 0x00;
+    public const int RECEIVE_BUFFER_REGISTER = 0x00;
+    public const int INTERRUPT_ENABLE_REGISTER = 0x01;
+    public const int IER_RX_DATA_READY = 0x01;
+    public const int IER_TX_HOLDING_REGISTER_EMPTY = 0x02;
+    public const int IER_LINE_STATUS = 0x04;
+    public const int IER_MODEM_STATUS = 0x08;
+    public const int INTERRUPT_ID_REGISTER = 0x02;
+    public const int IIR_MODEM_STATUS_INTERRUPT = 0x00;
+    public const int IIR_TX_HOLDING_REGISTER_INTERRUPT = 0x02;
+    public const int IIR_RX_DATA_READY_INTERRUPT = 0x04;
+    public const int IIR_LINE_STATUS_TIMEOUT = 0x06;
+    public const int IIR_FIFO_TIMEOUT = 0x0c;
+    public const int IIR_FIFO_ENABLED = 0xc0;
+    public const int FIFO_CONTROL_REGISTER = 0x02;
+    public const int FCR_FIFO_ENABLE = 0x01;
+    public const int FCR_RCVR_FIFO_RESET = 0x02;
+    public const int FCR_XMIT_FIFO_RESET = 0x04;
+    public const int FCR_TRIGGER_01 = 0x00;
+    public const int FCR_TRIGGER_04 = 0x40;
+    public const int FCR_TRIGGER_08 = 0x80;
+    public const int FCR_TRIGGER_16 = 0xc0;
+    public const int LINE_CONTROL_REGISTER = 0x03;
+    public const int LCR_DATA_BITS_5 = 0x00;
+    public const int LCR_DATA_BITS_6 = 0x01;
+    public const int LCR_DATA_BITS_7 = 0x02;
+    public const int LCR_DATA_BITS_8 = 0x03;
+    public const int LCR_STOP_BITS_1 = 0x00;
+    public const int LCR_STOP_BITS_2 = 0x04;
+    public const int LCR_PARITY_NONE = 0x00;
+    public const int LCR_PARITY_ODD = 0x08;
+    public const int LCR_PARITY_EVEN = 0x18;
+    public const int LCR_PARITY_MARK = 0x28;
+    public const int LCR_PARITY_SPACE = 0x38;
+    public const int LCR_SET_BREAK = 0x40;
+    public const int LCR_DLAB = 0x80;
+    public const int MODEM_CONTROL_REGISTER = 0x04;
+    public const int MCR_DTR = 0x01;
+    public const int MCR_RTS = 0x02;
+    public const int MCR_OUT1 = 0x04;
+    public const int MCR_OUT2 = 0x08;
+    public const int MCR_LOOPBACK = 0x10;
+    public const int LINE_STATUS_REGISTER = 0x05;
+    public const int LSR_DATA_READY = 0x01;
+    public const int LSR_OVERRUN_ERROR = 0x02;
+    public const int LSR_PARITY_ERROR = 0x04;
+    public const int LSR_FRAMING_ERROR = 0x08;
+    public const int LSR_BREAK_DETECT = 0x10;
+    public const int LSR_TRANSMITTER_BUFFER_EMPTY = 0x20;
+    public const int LSR_TRANSMITTER_EMPTY = 0x40;
+    public const int LSR_FIFO_DIRTY = 0x80;
+    public const int MODEM_STATUS_REGISTER = 0x06;
+    public const int MSR_DELTA_CTS = 0x01;
+    public const int MSR_DELTA_DSR = 0x02;
+    public const int MSR_DELTA_RI = 0x04;
+    public const int MSR_DELTA_CD = 0x08;
+    public const int MSR_CTS = 0x10;
+    public const int MSR_DSR = 0x20;
+    public const int MSR_RI = 0x40;
+    public const int MSR_CD = 0x80;
+    public const int DIVISOR_LATCH_LOW = 0x00;
+    public const int DIVISOR_LATCH_HIGH = 0x01;
+
+    public const int MODEM_STATUS_MASK = MSR_CTS | MSR_DSR | MSR_CD;
+
+    public const int UART_AUTO = 0;
+    public const int UART_8250 = 1;
+    public const int UART_16550 = 2;
+
+    public static int[] ISA_uarts = { 0x3f8, 0x2f8, 0x3e8, 0x2e8 };
+    public static int[] ISA_IRQs = { 4, 3, 4, 3 };
+
+    public struct ComPort
+    {
+        public ComPort* next;
+        public int uart;
+        public volatile byte modemStatus;
+        public byte modemStatusIgnore;
+        public byte lineStatus;
+        public byte bufferUsed;
+        public bool enabled;
+        public volatile bool statusUpdated;
+    }
+}
--- WinQuake/net_ser.cs
+++ WinQuake/net_ser.cs
@@ -45,7 +45,7 @@ public unsafe class net_ser_c
     public const int STATE_ESCAPE = 8;
     public const int STATE_ABORT = 9;
 
-    public static SerialLine serialLine[net_comx_c.NUM_COM_PORTS];
+    public static SerialLine* serialLine;
 
     public static int myDriverLevel;
 
@@ -186,7 +186,7 @@ public unsafe class net_ser_c
             {
                 case STATE_READY:
                     crc_c.CRC_Init(&p->crcValue);
-                    crc_c.CRC_ProcessByte(&p->crcValue, b);
+                    crc_c.CRC_ProcessByte(&p->crcValue, (byte)b);
 
                     if (p->client)
                     {
@@ -222,15 +222,15 @@ public unsafe class net_ser_c
 
                     if (p->mtype < MTYPE_ACK)
                     {
-                        p->sock->receiveMessage[p->sock->receiveMessageLength] = b;
+                        p->sock->receiveMessage[p->sock->receiveMessageLength] = (byte)b;
                         p->lengthFound++;
                     }
 
                     break;
 
                 case STATE_SEQUENCE:
-                    p->sequence = b;
-                    crc_c.CRC_ProcessByte(&p->crcValue, b);
+                    p->sequence = (byte)b;
+                    crc_c.CRC_ProcessByte(&p->crcValue, (byte)b);
 
                     if (p->mtype != MTYPE_ACK)
                     {
@@ -245,13 +245,275 @@ public unsafe class net_ser_c
 
                 case STATE_LENGTH1:
                     p->lengthStated = b * 256;
-                    crc_c.CRC_ProcessByte(&p->crcValue, b);
+                    crc_c.CRC_ProcessByte(&p->crcValue, (byte)b);
                     p->currState = STATE_LENGTH2;
                     break;
 
                 case STATE_LENGTH2:
                     p->lengthStated += b;
+                    crc_c.CRC_ProcessByte(&p->crcValue, (byte)b);
+
+                    if (p->mtype == MTYPE_RELIABLE && p->lengthStated > quakedef_c.MAX_MSGLEN)
+                    {
+                        p->currState = STATE_ABORT;
+                        console_c.Con_DPrintf($"Serial: bad reliable message length {p->lengthStated}\n");
+                    }
+                    else if (p->mtype == MTYPE_UNRELIABLE && p->lengthStated > quakedef_c.MAX_DATAGRAM)
+                    {
+                        p->currState = STATE_ABORT;
+                        console_c.Con_DPrintf($"Serial: bad unreliable message length {p->lengthStated}\n");
+                    }
+                    else
+                    {
+                        p->currState = STATE_DATA;
+
+                        if (p->mtype < MTYPE_ACK)
+                        {
+                            *(short*)&p->sock->receiveMessage[p->sock->receiveMessageLength + 1] = (short)p->lengthStated;
+                            p->lengthFound += 2;
+                        }
+                    }
+                    break;
+
+                case STATE_DATA:
+                    p->sock->receiveMessage[p->sock->receiveMessageLength + p->lengthFound] = (byte)b;
+                    p->lengthFound++;
+                    crc_c.CRC_ProcessByte(&p->crcValue, (byte)b);
+
+                    if (p->lengthFound == p->lengthStated + 3)
+                    {
+                        p->currState = STATE_CRC1;
+                    }
+                    break;
+
+                case STATE_CRC1:
+                    p->crcStated = (byte)(b * 256);
+                    p->currState = STATE_CRC2;
+                    break;
+
+                case STATE_CRC2:
+                    p->crcStated += (ushort)b;
+
+                    if (p->crcStated == crc_c.CRC_Value(p->crcValue))
+                    {
+                        p->currState = STATE_EOM;
+                    }
+                    else
+                    {
+                        p->currState = STATE_ABORT;
+                        console_c.Con_DPrintf("Serial: bad crc\n");
+                    }
+                    break;
+
+                case STATE_EOM:
+                    p->currState = STATE_ABORT;
+                    console_c.Con_DPrintf("Serial: bad message format\n");
+                    break;
+
+                case STATE_ABORT:
+                    break;
             }
         }
+
+        return 0;
+    }
+
+    public static int Serial_Init()
+    {
+        int n;
+
+        if (common_c.COM_CheckParm("-nolan") != 0)
+        {
+            return -1;
+        }
+
+        if (common_c.COM_CheckParm("-noserial") != 0)
+        {
+            return -1;
+        }
+
+        myDriverLevel = net_c.net_driverlevel;
+
+        if (net_comx_c.TTY_Init())
+        {
+            return -1;
+        }
+
+        for (n = 0; n < net_comx_c.NUM_COM_PORTS; n++)
+        {
+            serialLine[n].tty = net_comx_c.TTY_Open(n);
+            ResetSerialLineProtocol(&serialLine[n]);
+        }
+
+        console_c.Con_Printf("Serial driver initialized.\n");
+        net_c.serialAvailable = true;
+
+        return 0;
+    }
+
+    public static void Serial_Shutdown()
+    {
+        int n;
+
+        for (n = 0; n < net_comx_c.NUM_COM_PORTS; n++)
+        {
+            if (serialLine[n].connected)
+            {
+                Serial_Close(serialLine[n].sock);
+            }
+        }
+
+        net_comx_c.TTY_Shutdown();
+    }
+
+    public static void Serial_Listen(bool state)
+    {
+        listening = state;
+    }
+
+    public static bool Serial_CanSendMessage(net_c.qsocket_t* sock)
+    {
+        return sock->canSend;
+    }
+
+    public static bool Serial_CanSendUnreliableMessage(net_c.qsocket_t* sock)
+    {
+        return net_comx_c.TTY_OutputQueueIsEmpty(((SerialLine*)sock->driverdata)->tty);
+    }
+
+    public static int Serial_SendMessage(net_c.qsocket_t* sock, common_c.sizebuf_t* message)
+    {
+        SerialLine* p;
+        int n;
+        ushort crc;
+        byte b;
+
+        p = (SerialLine*)sock->driverdata;
+        crc_c.CRC_Init(&crc);
+
+        b = MTYPE_RELIABLE;
+
+        if (p->client)
+        {
+            b |= MTYPE_CLIENT;
+        }
+
+        net_comx_c.TTY_WriteByte(p->tty, b);
+        crc_c.CRC_ProcessByte(&crc, (byte)b);
+
+        b = (byte)p->sock->sendSequence;
+        net_comx_c.TTY_WriteByte(p->tty, b);
+
+        if (b == ESCAPE_COMMAND)
+        {
+            net_comx_c.TTY_WriteByte(p->tty, b);
+        }
+
+        crc_c.CRC_ProcessByte(&crc, (byte)b);
+
+        b = (byte)(message->cursize >> 8);
+        net_comx_c.TTY_WriteByte(p->tty, b);
+
+        if (b == ESCAPE_COMMAND)
+        {
+            net_comx_c.TTY_WriteByte(p->tty, b);
+        }
+
+        crc_c.CRC_ProcessByte(&crc, b);
+        b = (byte)(message->cursize & 0xff);
+        net_comx_c.TTY_WriteByte(p->tty, b);
+
+        if (b == ESCAPE_COMMAND)
+        {
+            net_comx_c.TTY_WriteByte(p->tty, b);
+        }
+
+        crc_c.CRC_ProcessByte(&crc, b);
+
+        for (n = 0; n < message->cursize; n++)
+        {
+            b = (byte)message->data[n];
+            net_comx_c.TTY_WriteByte(p->tty, b);
+
+            if (b == ESCAPE_COMMAND)
+            {
+                net_comx_c.TTY_WriteByte(p->tty, b);
+            }
+
+            crc_c.CRC_ProcessByte(&crc, b);
+        }
+
+        b = (byte)(crc_c.CRC_Value(crc) >> 8);
+        net_comx_c.TTY_WriteByte(p->tty, b);
+
+        if (b == ESCAPE_COMMAND)
+        {
+            net_comx_c.TTY_WriteByte(p->tty, b);
+        }
+
+        b = (byte)(crc_c.CRC_Value(crc) & 0xff);
+        net_comx_c.TTY_WriteByte(p->tty, b);
+
+        if (b == ESCAPE_COMMAND)
+        {
+            net_comx_c.TTY_WriteByte(p->tty, b);
+        }
+
+        net_comx_c.TTY_WriteByte(p->tty, ESCAPE_COMMAND);
+        net_comx_c.TTY_WriteByte(p->tty, ESCAPE_EOM);
+
+        net_comx_c.TTY_Flush(p->tty);
+
+        sock->canSend = false;
+        common_c.Q_memcpy(*sock->sendMessage, *message->data, message->cursize);
+        sock->sendMessageLength = message->cursize;
+        sock->lastSendTime = net_c.net_time;
+
+        return 1;
+    }
+
+    public static void ReSendMessage(net_c.qsocket_t* sock)
+    {
+        common_c.sizebuf_t temp;
+
+        console_c.Con_DPrintf("Serial: re-sending reliable\n");
+        temp.data = (int*)sock->sendMessage;
+        temp.maxsize = sock->sendMessageLength;
+        temp.cursize = sock->sendMessageLength;
+        Serial_SendMessage(sock, &temp);
+    }
+
+    public static int Serial_SendUnreliableMessage(net_c.qsocket_t* sock, common_c.sizebuf_t* message)
+    {
+        SerialLine* p;
+        int n;
+        ushort crc;
+        byte b;
+
+        p = (SerialLine*)sock->driverdata;
+        if (!net_comx_c.TTY_OutputQueueIsEmpty(p->tty))
+        {
+            net_comx_c.TTY_Flush(p->tty);
+            return 1;
+        }
+        crc_c.CRC_Init(&crc);
+
+        b = MTYPE_UNRELIABLE;
+        if (p->client)
+        {
+            b |= MTYPE_CLIENT;
+        }
+        net_comx_c.TTY_WriteByte(p->tty, b);
+        crc_c.CRC_ProcessByte(&crc, b);
+
+        b = (byte)p->sock->unreliableSendSequence;
+        p->sock->unreliableSendSequence = (uint)((b + 1) & 0xff);
+        net_comx_c.TTY_WriteByte(p->tty, b);
+        if (b == ESCAPE_COMMAND)
+        {
+            net_comx_c.TTY_WriteByte(p->tty, b);
+        }
+        crc_c.CRC_ProcessByte(&crc, b);
+
     }
 }
